3242651f12440c2498689c215bb13d6f97649ec1
albina_kazartseva <albinak74@gmail.com>
2025-01-26 17:47:06 +0500

    'index.html'

--- src/masks.py
+++ src/masks.py
@@ -1,24 +1,42 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    filename="../logs/masks.log",
+                    filemode="w", encoding="utf-8")
+
+# Создаем логеры для различных компонентов программы
+mask_loger = logging.getLogger("masks")
+
+
 def get_mask_card_number(card_number: int) -> str:
     """Функция get_mask_card_number принимает на вход номер карты и возвращает ее маску
-    :rtype: object
     """
+    mask_loger.debug("Начало работы функции по принятию номера карты")
     if not isinstance(card_number, int):
+        mask_loger.error("Ошибка ввода данных карты")
         raise TypeError("Ошибка ввода данных")
 
     if len(str(card_number)) == 16:
+        mask_loger.debug("Номер карты введен верно")
         return f"{str(card_number)[0:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
 
+    mask_loger.error("Неправильный номер карты")
     raise ValueError("Неправильный номер карты")
 
 
 def get_mask_account(account: int) -> str:
     """Функция get_mask_account принимает на вход номер счета и возвращает его маску"""
+    mask_loger.debug("Начало работы функции по принятию номера счета")
     if not isinstance(account, int):
+        mask_loger.error("Ошибка ввода данных счета")
         raise TypeError("Ошибка ввода данных")
 
     if len(str(account)) == 20:
+        mask_loger.debug("Номер счета введен верно")
         return f"**{str(account)[-4:]}"
 
+    mask_loger.error("Неправильный номер счета")
     raise ValueError("Неправильный номер счета")
 
 
@@ -26,3 +44,4 @@ def get_mask_account(account: int) -> str:
 if __name__ == "__main__":
     print(get_mask_card_number(7000792289606361))
     print(get_mask_account(73654108430135874305))
+    mask_loger.debug("Работа программы завершена")
--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,36 @@
 import json
 import os
 import typing
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    filename="../logs/utils.log",
+                    filemode="w", encoding="utf-8")
+
+# Создаем логеры для различных компонентов программы
+utils_loger = logging.getLogger("utils")
 
 
 def get_path_to_file(path_to_file: str) -> list[dict]:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
+    utils_loger.debug("Начало работы функции")
     if path_to_file:
+        utils_loger.debug("Файл указан")
         with open(path_to_file, "r", encoding="utf-8") as file:
             transactions_list = json.load(file)
             if transactions_list:
+                utils_loger.debug("Файл загружен")
                 return transactions_list
+
+        utils_loger.error("Файл не открылся")
         return []
 
     else:
+        utils_loger.error("Файл не указан")
         return []
 
 
 if __name__ == "__main__":
     print(get_path_to_file("../data/operations.json"))
+    utils_loger.debug("Работа программы завершена")
